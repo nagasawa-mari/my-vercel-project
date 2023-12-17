156b4f44f7c3e187e2785ea0b4acf432d132f41c
nagasawa-mari <148606075+nagasawa-mari@users.noreply.github.com>
2023-12-17 10:57:08 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
-"use client"
-import { FormEvent, useState } from "react"
+import { FormEvent, useState } from "react";
 
-type task = {
-  id: number
-  title: string
-  end_date: string
-  done: boolean
-}
+type Task = {
+  id: number;
+  title: string;
+  end_date: string;
+  done: boolean;
+};
 
-const demoTasks: task[] = [
+const demoTasks: Task[] = [
   {
     id: 1,
     title: "牛乳を買う",
@@ -27,45 +26,51 @@ const demoTasks: task[] = [
     end_date: "2021-06-30",
     done: true,
   },
-]
+];
 
 export default function Page() {
-
-  const [tasks, setTasks] = useState<task[]>(demoTasks)
+  const [tasks, setTasks] = useState<Task[]>(demoTasks);
 
   function addTask(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault()
+    event.preventDefault();
 
-    const formData = new FormData(event.currentTarget)
+    const formData = new FormData(event.currentTarget);
 
-    const title = formData.get("title") as string //牛乳を買う
-    const endData = formData.get("end_date") as string // YYY-MM-DD
+    const title = formData.get("title") as string;
+    const endDate = formData.get("end_date") as string;
 
-  setTasks([...tasks, {
-    id: tasks.length + 1,
-    title: title,
-    end_date: endDate,
-    done: false,
-  }])
-}
+    setTasks([
+      ...tasks,
+      {
+        id: tasks.length + 1,
+        title: title,
+        end_date: endDate,
+        done: false,
+      },
+    ]);
+  }
 
   return (
     <div className="p-8">
       <h1 className="mb-4">ToDo管理アプリ</h1>
 
-     <form className="mb-8 space-x4" onSubmit={(addTask)}>
-      <input type={"text"} name={"title"} />
-      <input type={"date"} name={"end_date"}/>
-      <input type="submit" value="Submit"/>
+      <form className="mb-8 space-x-4" onSubmit={addTask}>
+        <input type="text" name="title" />
+        <input type="date" name="end_date" />
+        <input type="submit" value="Submit" />
       </form>
-  
+
       {tasks.map((item) => (
         <div key={item.id} className="flex gap-4">
-          <input type="checkbox" defaultChecked={item.done} onChange={() => (console.log("submit"))} />
+          <input
+            type="checkbox"
+            defaultChecked={item.done}
+            onChange={() => console.log("submit")}
+          />
           <span>{item.title}</span>
           <span>{item.end_date}</span>
         </div>
       ))}
     </div>
-  )
+  );
 }
